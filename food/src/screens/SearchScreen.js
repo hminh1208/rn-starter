3ecbf01a00513f2ebcd33d4dae0441d1500a901e
Phan Hong Minh - IT3 <phminh@vnpt.vn>
2024-01-17 22:17:37 +0700

    +finish section 9

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -2,19 +2,40 @@ import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
 import { useState } from "react";
+import yelp from "../api/yelp";
 
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
+    const [results, setResults] = useState([]);
+    const [errorMsg, setErrorMsg] = useState('');
+
+    const searchApi = async () => {
+        try {
+            const response = await yelp.get("/search", {
+                params: {
+                    limit: 50,
+                    term,
+                    location: "san jose",
+                },
+            });
+
+            setResults(response.data.businesses);
+        } catch (error) {
+            //console.log(error.toJSON())
+
+            setErrorMsg('Something went wrong')
+        }
+    };
 
     return (
         <View style={styles.background}>
             <SearchBar
                 term={term}
                 onSearchTermChange={(newTerm) => setTerm(newTerm)}
-                onTermSubmit={() => console.log('Term was submitted')}
+                onTermSubmit={searchApi}
             />
 
-            <Text>{term}</Text>
+            {errorMsg ? <Text>{errorMsg}</Text> : <Text>We found {results.length} results</Text>}
         </View>
     );
 };
@@ -23,7 +44,7 @@ const styles = StyleSheet.create({
     background: {
         backgroundColor: "#FFF",
         height: "100%",
-        padding: 10
+        padding: 10,
     },
 });
 
